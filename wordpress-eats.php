fc33a4709c3197209bf33c20cb8b04b8e8cfc160
darkiii <CDJ2GZr7xEZRwMB>
2022-08-02 01:07:31 +0200

    Initial commit

---
+++ wordpress-eats.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name:       Wordpress Eats
+ * Plugin URI:        https://zagroba.eu/
+ * Description:       Start using Wordpress as your online Food Merchant
+ * Version:           0.1.0
+ * Requires at least: 4.7
+ * Requires PHP:      7.0
+ * Author:            Adam Zagroba
+ * Author URI:        https://zagroba.eu/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-eats
+ * Domain Path:       /languages
+ */
+
+require_once "load.php";
