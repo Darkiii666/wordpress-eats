c706c82b35e5f20fddda6ca5601806955ad544cd
darkiii <CDJ2GZr7xEZRwMB>
2022-08-03 21:14:14 +0200

    Another endpoints in header and customizer

--- customizer.php
+++ customizer.php
@@ -1,6 +1,6 @@
 <?php
 namespace wp_eats;
-function wp_eats_customizer($wp_customize)
+function customizer_init($wp_customize)
 {
 
     $wp_customize->add_section('wp-eats-settings', array(
@@ -10,27 +10,77 @@ function wp_eats_customizer($wp_customize)
         'capability' => 'edit_theme_options',
     ));
 
-
+    $wp_customize->add_setting("wp-eats-dashboard-site");
+    $wp_customize->add_control("wp-eats-dashboard-site", array(
+            "type" => "dropdown-pages",
+            "priority" => 10,
+            "section" => "wp-eats-settings",
+            'label' => __( 'Dashboard Site', "wp-eats" ),
+            'description' => __( '', "wp-eats" ),
+            'allow_addition' => "true"
+        )
+    );
+    $wp_customize->add_setting("wp-eats-coupons-site");
+    $wp_customize->add_control("wp-eats-coupons-site", array(
+            "type" => "dropdown-pages",
+            "priority" => 10,
+            "section" => "wp-eats-settings",
+            'label' => __( 'Coupons Site', "wp-eats" ),
+            'description' => __( '', "wp-eats" ),
+            'allow_addition' => "true"
+        )
+    );
     $wp_customize->add_setting("wp-eats-invoices-site");
     $wp_customize->add_control("wp-eats-invoices-site", array(
             "type" => "dropdown-pages",
             "priority" => 10,
             "section" => "wp-eats-settings",
-            'label' => __( 'Site with Invoices', "wp-eats" ),
-            'description' => __( 'Where list with invoices should be displayed', "wp-eats" ),
+            'label' => __( 'Invoices Site', "wp-eats" ),
+            'description' => __( '', "wp-eats" ),
+            'allow_addition' => "true"
+        )
+    );
+    $wp_customize->add_setting("wp-eats-restaurants-site");
+    $wp_customize->add_control("wp-eats-restaurants-site", array(
+            "type" => "dropdown-pages",
+            "priority" => 10,
+            "section" => "wp-eats-settings",
+            'label' => __( 'Restaurants', "wp-eats" ),
+            'description' => __( '', "wp-eats" ),
+            'allow_addition' => "true"
+        )
+    );
+    $wp_customize->add_setting("wp-eats-users-site");
+    $wp_customize->add_control("wp-eats-users-site", array(
+            "type" => "dropdown-pages",
+            "priority" => 10,
+            "section" => "wp-eats-settings",
+            'label' => __( 'Users', "wp-eats" ),
+            'description' => __( '', "wp-eats" ),
+            'allow_addition' => "true"
+        )
+    );
+    $wp_customize->add_setting("wp-eats-orders-site");
+    $wp_customize->add_control("wp-eats-orders-site", array(
+            "type" => "dropdown-pages",
+            "priority" => 10,
+            "section" => "wp-eats-settings",
+            'label' => __( 'Orders', "wp-eats" ),
+            'description' => __( '', "wp-eats" ),
             'allow_addition' => "true"
         )
     );
 
+
 }
 
 
 
 
-function wp_eats_customizer_save()
+function customizer_save()
 {
 
 }
 
-add_action('customize_register', 'wp_eats_customizer');
-add_action('customize_save_after', 'wp_eats_customizer_save');
+add_action('customize_register', 'wp_eats\customizer_init');
+add_action('customize_save_after', 'wp_eats\customizer_save');
