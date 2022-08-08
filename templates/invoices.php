195a1f3db43dcdbd6e8d879cd6e0401b6e9f134f
darkiii <CDJ2GZr7xEZRwMB>
2022-08-09 01:14:26 +0200

    Invoice list split into smaller parts

--- templates/invoices.php
+++ templates/invoices.php
@@ -10,66 +10,10 @@ include "parts/header.php";?>
     </div>
     <form action="" method="GET">
         <?php wp_nonce_field( 'wp_eats_invoice_list', 'wp_eats_nonce' );
+        include "parts/invoice-filters.php";
         ?>
 
-        <div class="wp-eats__filters wp-eats__filters--invoices row my-3 g-3 align-items-end">
-            <div class="wp-eats__status-filters col col-md-4 flex-fill align-self-start">
-                <?php
-                $statuses = array("all" => __("All", "wp-eats"));
-                $statuses = array_merge($statuses, Invoice::get_invoice_statuses());
-                if (isset($_REQUEST["invoice-status"]) && isset($statuses[$_REQUEST['invoice-status']])){
-                    $current_status = $_REQUEST['invoice-status'];
-                } else {
-                    $current_status = "all";
-                }
-                foreach ($statuses as $status_key => $status):?>
 
-                    <span class="wp-eats__status-filter">
-                        <input autocomplete="off" class="btn-check" id="filter_status_<?php echo esc_attr($status_key);?>" type="radio" name="invoice_status" value="<?php echo $status_key?>"<?php if($status_key == $current_status) echo ' checked="true"'?>>
-                        <label for="filter_status_<?php echo esc_attr($status_key);?>" class="btn btn-sm wp-eats__status-filter-text"><?php echo $status?></label>
-                    </span>
-                <?php endforeach;?>
-
-            </div>
-            <div class="wp-eats__date-filters col-md-3">
-                <?php
-                if (@strtotime($_REQUEST['date-from']) && @strtotime($_REQUEST['date-to'])) {
-                    $date_from = $_REQUEST["date-from"];
-                    $date_to = $_REQUEST["date-to"];
-                } else {
-                    $date_from = date(WP_Eats::get_format_date());
-                    $date_to = date(WP_Eats::get_format_date(), strtotime('midnight +30 day', time()));
-                }
-                ?>
-                <div class="input-group wp-eats__date-filters-wrap">
-                    <span class="input-group-text">
-                        <span class="me-2 lh-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
-  <path fill="#6c7481" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-</svg>
-                        </span>
-                         <?php _ex("From", "Invoices from date", "wp-eats")?></span>
-                    <input class="form-control" name="date-range" type="text" value="<?php echo esc_attr($date_from) . " → " . esc_attr($date_to)?>">
-                </div>
-
-
-            </div>
-            <div class="wp-eats__search-filter col">
-                <div class="input-group">
-
-
-                <span class="input-group-text bg-white pe-0">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path fill="#6c7481" d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg>
-                           </span>
-                <input class="form-control border-start-0" type="text" name="search-name" value="<?php echo @esc_attr($_REQUEST['search-name'])?>" placeholder="<?php _e("Search", "wp-eats")?>">
-                </div>
-            </div>
-            <div class="wp-eats__actions col">
-                <button class="btn btn-primary button button--mark-as-paid"><?php _e("Mark as paid")?></button>
-            </div>
-        </div>
         <div class="wp-eats__list wp-eats__list--invoices">
             <table class="table">
                 <thead>
@@ -129,50 +73,7 @@ include "parts/header.php";?>
                 <tfoot>
                 <tr>
                     <td colspan="15">
-                        <div class="wp-eats__table-footer">
-                            <div class="wp-eats__table--page-counter">
-                                <?php
-                                $paged = get_query_var('paged');
-                                if ($paged < 2) $paged = 1;
-                                echo __("Page", "wp-eats") . ' ' . $paged . ' ' . __("of", "wp-eats") . ' ' . $invoices_query->max_num_pages;
-                                ;?>
-                            </div>
-                            <div class="wp-eats__table-pagination">
-                                <?php $pagination = paginate_links( array(
-                                    'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
-                                    'total'        => $invoices_query->max_num_pages,
-                                    'current'      => max( 1, get_query_var( 'paged' ) ),
-                                    'format'       => '?paged=%#%',
-                                    'show_all'     => true,
-                                    'type'         => 'plain',
-                                    'end_size'     => 2,
-                                    'mid_size'     => 1,
-                                    'prev_next'    => true,
-                                    'prev_text'    => sprintf( '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-</svg>'),
-                                    'next_text'    => sprintf( '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg>' ),
-                                    'add_args'     => true,
-                                    'add_fragment' => '',
-                                ) );
-                                if ($paged < 2):?>
-                                    <span class="prev page-numbers"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path>
-                                        </svg></span>
-                                <?php
-                                endif;
-                                echo $pagination;
-                                if ($paged == $invoices_query->max_num_pages):?>
-                                    <span class="next page-numbers"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
-                                        </svg></span>
-                                <?php
-                                endif;
-                                ?>
-                            </div>
-                        </div>
+                        <?php include "parts/invoice-table-footer.php";?>
 
                     </td>
                 </tr>
---
+++ templates/parts/invoice-table-footer.php
@@ -0,0 +1,44 @@
+<div class="wp-eats__table-footer">
+    <div class="wp-eats__table--page-counter">
+        <?php
+        $paged = get_query_var('paged');
+        if ($paged < 2) $paged = 1;
+        echo __("Page", "wp-eats") . ' ' . $paged . ' ' . __("of", "wp-eats") . ' ' . $invoices_query->max_num_pages;
+        ;?>
+    </div>
+    <div class="wp-eats__table-pagination">
+        <?php $pagination = paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $invoices_query->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => true,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+</svg>'),
+            'next_text'    => sprintf( '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+</svg>' ),
+            'add_args'     => true,
+            'add_fragment' => '',
+        ) );
+        if ($paged < 2):?>
+            <span class="prev page-numbers"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path>
+                                        </svg></span>
+        <?php
+        endif;
+        echo $pagination;
+        if ($paged == $invoices_query->max_num_pages):?>
+            <span class="next page-numbers"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                                        </svg></span>
+        <?php
+        endif;
+        ?>
+    </div>
+</div>
